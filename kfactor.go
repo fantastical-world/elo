2b1ae53484f9e772a044009eb3dde876bb796149
Isaac Gomez <eyeszack@me.com>
2021-05-10 23:18:51 -0700

    added some kfactor calcs from USCF

---
+++ kfactor.go
@@ -0,0 +1,17 @@
+package elo
+
+//USCFKFactorFromRating provides a kfactor from a player's rating.
+func (e *ELOCalculator) USCFKFactorFromRating(rating float64) float64 {
+	if rating < 2100 {
+		return 32
+	}
+	if rating > 2400 {
+		return 16
+	}
+	return 24
+}
+
+//USCFKFactorFromGamesPlayed provides a kfactor from the amount of games played.
+func (e *ELOCalculator) USCFKFactorFromGamesPlayed(pp, pt int) float64 {
+	return 800 / float64(pp+pt)
+}
